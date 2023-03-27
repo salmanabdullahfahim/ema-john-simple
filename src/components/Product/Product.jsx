9f9e06271bbc4c9b1449a4a86e3f0b9d92eb721a
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-03-27 12:50:08 +0600

    favicon and title added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import './Product.css'
+import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Product = (props) => {
-    const { name, img, price, quantity, seller, ratings } = props.product
+    const { name, img, price, quantity, seller, ratings,id } = props.product;
+    const handleAddToCart = props.handleAddToCart;
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -12,7 +15,9 @@ const Product = (props) => {
                 <p>Manufacturer: {seller}</p>
                 <p>Rating: {ratings} stars</p>
             </div>
-            <button className='btn-cart'>Add to Cart</button>
+            <button onClick={()=>handleAddToCart(props.product)} className='btn-cart'>Add to Cart 
+            <FontAwesomeIcon icon={faShoppingCart} />
+            </button>
         </div>
     );
 };
